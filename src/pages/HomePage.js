d7c59cd98308d3a4aca69447039590ed518aed65
Anurag <anuragmarketing101@gmail.com>
2022-10-20 12:48:30 +0530

    added-testing-log-in-homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from '../components/HomePage/Banner/Banner'
 
 function HomePage() {
+  console.log('testing anurag/coinlist branch');
   return (
     <div className='w-full  '>
       <Banner/>
